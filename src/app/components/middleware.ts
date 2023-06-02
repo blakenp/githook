991a1f5608e0636edf46ecd2dce51f011c94059d
Blake Peterson <blake.ptrsn18@gmail.com>
2023-06-01 18:18:51 -0600

    Add middleware and api endpoint with request handlers

---
+++ src/app/components/middleware.ts
@@ -0,0 +1,18 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+const allowedOrigins = ['https://api-request-two.vercel.app', 'http://localhost:3000']
+
+function middleware(response: NextResponse, request: NextRequest) {
+  const requestHeaders = new Headers(request.headers)
+  const origin = requestHeaders.get('origin')
+
+  if (origin && allowedOrigins.includes(origin)) {
+    response.headers.set('Access-Control-Allow-Origin', origin)
+  }
+
+  response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+  response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  response.headers.set('Access-Control-Max-Age', '86400')
+}
+
+export default middleware
